d3f855caed6a83d5bbc0f8bc6c23acd50cab2853
Ashishroy1724 <ashishroy78782@gmail.com>
2025-05-23 13:29:44 +0530

    Necessary Lib added and files made, also Model code written

---
+++ backend/src/Models/AuthModel.js
@@ -0,0 +1,27 @@
+import mongoose from 'mongoose';
+
+const authSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+    },
+    dob: {
+        type: Date,
+        required: true,
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+},
+{
+    timestamps: true,
+});
+
+const AuthModel = mongoose.model('Auth', authSchema);
+export default AuthModel;
